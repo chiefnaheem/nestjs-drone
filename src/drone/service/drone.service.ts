de778ff502ed4f8ae878af4708cae9411eacc7d1
naheem adedokun <nadedokun@heckerbella.com>
2022-12-06 09:51:01 +0100

    updates

--- src/drone/service/drone.service.ts
+++ src/drone/service/drone.service.ts
@@ -15,7 +15,7 @@ export class DroneService {
   constructor(
     @InjectModel(DroneEntity.name)
     private readonly droneModel: Model<DroneDocument>,
-    private medicationService: MedicationService
+    private medicationService: MedicationService,
   ) {}
 
   //register drone
@@ -78,43 +78,35 @@ export class DroneService {
     }
   }
 
-  //we want to load a drone with medication items
-  //Prevent the drone from being loaded with more weight that it can carry;
-  // Prevent the drone from being in LOADING state if the battery level is **below 25%**;
-  //change the state to LOADING when loading
-  //decrement batteryCapacity periodically
-    
-    async loadDrone(id: string, medication: string[]): Promise<DroneEntity> {
+
+  async loadDrone(id: string, medication: string[]): Promise<DroneEntity> {
     try {
       const drone = await this.droneModel.findById(id);
-        if (!drone) {
-            throw new NotFoundException('Drone not found');
-        }
-        if (drone.state === 'LOADING') {
-            throw new BadRequestException('Drone is already loading');
-        }
-        if (drone.batteryCapacity < 25) {
-            throw new BadRequestException('Drone battery is low');
-        }
-        //we will loop through medication input and get them by id, then check for their weight to compare with the weight limit of the drone
-        let totalWeight = 0;
-        for (let key of medication) {
-            const med = await this.medicationService.getMedicationById(key);
-            totalWeight += med.weight;
-        }
-        if (totalWeight > drone.weightLimit) {
-            throw new BadRequestException('Drone weight limit exceeded');
-        }
-        //if all is good, we will update the drone state to LOADING and decrement the batteryCapacity
-        drone.state = 'LOADING';
-        drone.batteryCapacity -= 5;
-        await drone.save();
-        return drone;
+      if (!drone) {
+        throw new NotFoundException('Drone not found');
+      }
+      if (drone.state === 'LOADING') {
+        throw new BadRequestException('Drone is already loading');
+      }
+      if (drone.batteryCapacity < 25) {
+        throw new BadRequestException('Drone battery is low');
+      }
+      
+      let totalWeight = 0;
+      for (const key of medication) {
+        const med = await this.medicationService.getMedicationById(key);
+        totalWeight += med.weight;
+      }
+      if (totalWeight > drone.weightLimit) {
+        throw new BadRequestException('Drone weight limit exceeded');
+      }
+      //if all is good, we will update the drone state to LOADING and decrement the batteryCapacity
+      drone.state = 'LOADING';
+      drone.batteryCapacity -= 5;
+      await drone.save();
+      return drone;
     } catch (error) {
-        throw new InternalServerErrorException(error.message);
+      throw new InternalServerErrorException(error.message);
     }
-}
-
-
-
+  }
 }
