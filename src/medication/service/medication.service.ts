4d8a1cd5a14bbd348e603ebc1fe95cfc57513faf
naheem adedokun <nadedokun@heckerbella.com>
2022-12-06 08:37:25 +0100

    get medicatioms

--- src/medication/service/medication.service.ts
+++ src/medication/service/medication.service.ts
@@ -1,37 +1,56 @@
-import { BadRequestException,
+import {
+  BadRequestException,
   Injectable,
   InternalServerErrorException,
-  NotFoundException } from '@nestjs/common';
+  NotFoundException,
+} from '@nestjs/common';
 import mongoose, { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
-import { MedicationDocument, MedicationEntity } from '../schema/medication.schema';
+import {
+  MedicationDocument,
+  MedicationEntity,
+} from '../schema/medication.schema';
 
 @Injectable()
 export class MedicationService {
-    constructor(
-        @InjectModel(MedicationEntity.name)
-        private readonly medicationModel: Model<MedicationDocument>,
-    ){}
+  constructor(
+    @InjectModel(MedicationEntity.name)
+    private readonly medicationModel: Model<MedicationDocument>,
+  ) {}
 
-    //create medication
-    async createMedication(medication: MedicationEntity): Promise<MedicationEntity> {
+  //create medication
+  async createMedication(
+    medication: MedicationEntity,
+  ): Promise<MedicationEntity> {
+    try {
+      const newMedication = new this.medicationModel(medication);
+      return newMedication.save();
+    } catch (error) {
+      throw new InternalServerErrorException(error.message);
+    }
+  }
+
+  //get all medications
+  async getAllMedications(): Promise<MedicationEntity[]> {
+    try {
+      return this.medicationModel.find().exec();
+    } catch (error) {
+      throw new InternalServerErrorException(error.message);
+    }
+  }
+
+    //get medication by id
+    async getMedicationById(id: string): Promise<MedicationEntity> {
         try {
-            const newMedication = new this.medicationModel(medication);
-            return newMedication.save();
-        }
-        catch (error) {
+            const medication = await this.medicationModel
+                .findById(id)
+                .exec();
+            if (!medication) {
+                throw new NotFoundException('Medication not found');
+            }
+            return medication;
+        } catch (error) {
             throw new InternalServerErrorException(error.message);
         }
     }
-
-    //get all medications
-    async getAllMedications(): Promise<MedicationEntity[]> {
-        try {
-            return this.medicationModel.find().exec();
-        }
-        catch (error) {
-            throw new InternalServerErrorException(error.message);
-        }
-    }
-
 }
