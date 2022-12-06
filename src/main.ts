b53a0988136f3532e831ede1b8b717587a679a17
naheem adedokun <nadedokun@heckerbella.com>
2022-12-06 06:48:07 +0100

    validation pipe added globally

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors();
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,  
+    }),
+    );
+  const port = process.env.PORT
+  await app.listen(port, () => {
+    Logger.debug(`app listening on port ${port}`);
+  });
 }
 bootstrap();
