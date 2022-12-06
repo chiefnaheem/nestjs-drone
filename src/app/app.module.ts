fdaf55ee0be8d0de4e33c68eb8dddfc37bf849e5
naheem adedokun <nadedokun@heckerbella.com>
2022-12-06 06:45:54 +0100

    database setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from './controller/app.controller';
+import { AppService } from './service/app.service';
+import { TerminusModule } from '@nestjs/terminus';
+import { HttpModule } from '@nestjs/axios';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { envConfig } from '../config/constant/env.configuration';
+import { configValidation } from '../config/validator/config.validator';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>(envConfig.CONNECTION_STRING),
+      }),
+      inject: [ConfigService],
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      cache: false,
+      validationSchema: configValidation,
+      envFilePath: ['.env'],
+    }),
+    TerminusModule,
+    HttpModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config/constant/env.configuration.ts
@@ -0,0 +1,5 @@
+export const envConfig = {
+  PORT: 'PORT',
+  NODE_ENV: 'NODE_ENV',
+  CONNECTION_STRING: 'CONNECTION_STRING',
+};
