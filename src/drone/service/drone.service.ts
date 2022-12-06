e4ee2be0dca7256b515005cc3089eee9151c2e22
naheem adedokun <nadedokun@heckerbella.com>
2022-12-06 08:26:49 +0100

    register drone

--- src/drone/service/drone.service.ts
+++ src/drone/service/drone.service.ts
@@ -1,6 +1,11 @@
-import { Injectable } from '@nestjs/common';
-import mongoose, { Model } from 'mongoose';
+import { BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException } from '@nestjs/common';
+import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
+import { DroneDocument, DroneEntity } from '../schema/drone.schema';
+import { RegisterDroneDto } from '../dto/drone.dto';
 
 @Injectable()
 export class DroneService {
@@ -8,4 +13,15 @@ export class DroneService {
         @InjectModel(DroneEntity.name)
         private readonly droneModel: Model<DroneDocument>,
     ){}
+
+    //register drone
+    async registerDrone(drone: RegisterDroneDto): Promise<DroneEntity> {
+        try {
+            const newDrone = new this.droneModel(drone);
+            return newDrone.save();
+        }
+        catch (error) {
+            throw new InternalServerErrorException(error.message);
+        }
+    }
 }
